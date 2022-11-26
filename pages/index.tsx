1cd8dcafc4f79700ea09e25396f392c08d4560e7
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-26 00:44:52 +0000

    fix: missing tokens.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Tokens from "../json/Token.json";
 import Blockchains from "../json/Blockchain.json";
 import Projects from "../json/Project.json";
 import { useEffect, useState } from "react";
