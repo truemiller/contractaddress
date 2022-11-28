496502fbb483572816e06d6601bbc3546bbdfe81
Josh <31908788+truemiller@users.noreply.github.com>
2022-11-28 21:43:55 +0000

    updated avalanche wavax contracts

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -24,7 +24,11 @@ const sitemapApi = async (req: NextApiRequest, res: NextApiResponse) => {
     </urlset>`
 
   const xmlTop = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>http://www.example.com/foo.html</loc>
+    </url>
+    `
 
   let xmlUrls = ''
 
