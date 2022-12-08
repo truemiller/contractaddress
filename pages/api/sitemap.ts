9f53889df7e90f9dc2acbdd43d9a6adf851bb694
Josh <31908788+truemiller@users.noreply.github.com>
2022-12-08 23:46:19 +0000

    remove redundant xml

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -15,14 +15,6 @@ const sitemapApi = async (req: NextApiRequest, res: NextApiResponse) => {
   // Instructing the Vercel edge to cache the file
   res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600')
 
-  // generate sitemap here
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"> 
-    <url>
-      <loc>http://www.example.com/foo.html</loc>
-    </url>
-    </urlset>`
-
   const xmlTop = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
